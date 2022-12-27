944a0cce721b4470de896bb7af241e215e4e945d
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2022-12-27 19:25:19 +0530

    Android Studio project for hostel-debt-book of 4 hostellers

---
+++ app/src/main/java/com/example/hostel_pttu_book/MainActivity.java
@@ -0,0 +1,460 @@
+package com.example.hostel_pttu_book;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private Button saveButton;
+    private List<String>list;
+    String[] permission = {READ_EXTERNAL_STORAGE,WRITE_EXTERNAL_STORAGE};
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    EditText amtValue,adInd,asInd,baInd,jeInd;
+    Button hist_button,note_button,clrButton,calcButton,btnPlsAmt,btnMinsAmt,adPlsBtn,asPlsBtn,adMinsBtn,asMinsBtn,baPlsBtn,jePlsBtn,baMinsBtn,jeMinsBtn;
+    TextView adAmt,asAmt,baAmt,jeAmt;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String adTEXT = "adText";
+    public static final String asTEXT = "asText";
+    public static final String baTEXT = "baText";
+    public static final String jeTEXT = "jeText";
+    public static final String curdate = "curdate";
+
+    private String filename = ".demoFile.txt";
+
+    private String adText;
+    private String asText;
+    private String baText;
+    private String jeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        amtValue=(EditText) findViewById(R.id.amtValue);
+
+        adInd=(EditText) findViewById(R.id.adInd);
+        asInd=(EditText) findViewById(R.id.asInd);
+        baInd=(EditText) findViewById(R.id.baInd);
+        jeInd=(EditText) findViewById(R.id.jeInd);
+
+        btnPlsAmt=(Button) findViewById(R.id.btnPlsAmt);
+        btnMinsAmt=(Button) findViewById(R.id.btnMinsAmt);
+
+        adPlsBtn=(Button) findViewById(R.id.adPlsBtn);
+        adMinsBtn=(Button) findViewById(R.id.adMinsBtn);
+
+        asPlsBtn=(Button) findViewById(R.id.asPlsBtn);
+        asMinsBtn=(Button) findViewById(R.id.asMinsBtn);
+
+        baPlsBtn=(Button) findViewById(R.id.baPlsBtn);
+        baMinsBtn=(Button) findViewById(R.id.baMinsBtn);
+
+        jePlsBtn=(Button) findViewById(R.id.jePlsBtn);
+        jeMinsBtn=(Button) findViewById(R.id.jeMinsBtn);
+
+        saveButton = (Button) findViewById(R.id.save_button);
+        clrButton = (Button) findViewById(R.id.clr_button);
+        calcButton = (Button) findViewById(R.id.calc_button);
+        note_button = (Button) findViewById(R.id.note_button);
+        hist_button = (Button) findViewById(R.id.hist_button);
+
+        adAmt=(TextView) findViewById(R.id.adAmt);
+        asAmt=(TextView) findViewById(R.id.asAmt);
+        baAmt=(TextView) findViewById(R.id.baAmt);
+        jeAmt=(TextView) findViewById(R.id.jeAmt);
+
+
+        adPlsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad1=addition(adInd.getText().toString(),adAmt.getText().toString());
+                adAmt.setText(ad1);
+                adInd.setText("0");
+            }
+        });
+        adMinsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad2=substraction(adInd.getText().toString(),adAmt.getText().toString());
+                adAmt.setText(ad2);
+                adInd.setText("0");
+            }
+        });
+
+        asPlsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad1=addition(asInd.getText().toString(),asAmt.getText().toString());
+                asAmt.setText(ad1);
+                asInd.setText("0");
+            }
+        });
+        asMinsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad2=substraction(asInd.getText().toString(),asAmt.getText().toString());
+                asAmt.setText(ad2);
+                asInd.setText("0");
+            }
+        });
+
+        baPlsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad1=addition(baInd.getText().toString(),baAmt.getText().toString());
+                baAmt.setText(ad1);
+                baInd.setText("0");
+            }
+        });
+        baMinsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad2=substraction(baInd.getText().toString(),baAmt.getText().toString());
+                baAmt.setText(ad2);
+                baInd.setText("0");
+            }
+        });
+
+        jePlsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad1=addition(jeInd.getText().toString(),jeAmt.getText().toString());
+                jeAmt.setText(ad1);
+                jeInd.setText("0");
+            }
+        });
+        jeMinsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ad2=substraction(jeInd.getText().toString(),jeAmt.getText().toString());
+                jeAmt.setText(ad2);
+                jeInd.setText("0");
+            }
+        });
+
+        btnPlsAmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double amt=0,n1;
+                n1=Double.parseDouble(amtValue.getText().toString());
+                amt=n1/4;
+                amtValue.setText("0");
+
+                String ad1=addition(adAmt.getText().toString(),amt);
+                adAmt.setText(ad1);
+
+                String as1=addition(asAmt.getText().toString(),amt);
+                asAmt.setText(as1);
+
+                String ba1=addition(baAmt.getText().toString(),amt);
+                baAmt.setText(ba1);
+
+                String je1=addition(jeAmt.getText().toString(),amt);
+                jeAmt.setText(je1);
+            }
+        });
+        btnMinsAmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double amt=0,n1;
+                n1=Double.parseDouble(amtValue.getText().toString());
+                amt=n1;
+                amtValue.setText("0");
+
+                String ad1=substraction(adAmt.getText().toString(),amt);
+                adAmt.setText(ad1);
+
+                String as1=substraction(asAmt.getText().toString(),amt);
+                asAmt.setText(as1);
+
+                String ba1=substraction(baAmt.getText().toString(),amt);
+                baAmt.setText(ba1);
+
+                String je1=substraction(jeAmt.getText().toString(),amt);
+                jeAmt.setText(je1);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkPermission()){
+                    saveData();
+                }else{
+                    requestPermission();
+                }
+            }
+        });
+        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+               if (result.getResultCode()== Activity.RESULT_OK){
+                   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+                       if (Environment.isExternalStorageManager()){
+                           printMessage("Permission Granted");
+                       }else{
+                           printMessage("permission Denied");
+                       }
+                   }
+               }
+            }
+        });
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+        clrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAlertDialoge();
+            }
+        });
+        note_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity3.class));
+            }
+        });
+        hist_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity4.class));
+            }
+        });
+
+        loadData();
+        updateViews();
+    }
+
+    void requestPermission(){
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setData(Uri.parse(String.format("package:%s",new Object[]{getApplicationContext().getPackageName()})));
+                activityResultLauncher.launch(intent);
+            }catch(Exception e){
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                activityResultLauncher.launch(intent);
+            }
+        }else{
+            ActivityCompat.requestPermissions(MainActivity.this,permission,30);
+
+        }
+    }
+    boolean checkPermission(){
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+            return Environment.isExternalStorageManager();
+        }else{
+            int readcheck= ContextCompat.checkSelfPermission(getApplicationContext(),READ_EXTERNAL_STORAGE);
+            int writecheck= ContextCompat.checkSelfPermission(getApplicationContext(),WRITE_EXTERNAL_STORAGE);
+            return readcheck ==PackageManager.PERMISSION_GRANTED && writecheck ==PackageManager.PERMISSION_GRANTED;
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,int [] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case 30:
+                if (grantResults.length>0){
+                    boolean readper = grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                    boolean writeper=grantResults[1]==PackageManager.PERMISSION_GRANTED;
+                    if (readper && writeper){
+                        printMessage("Permission Granted..!");
+                    }else{
+                        printMessage("Permission Denied");
+                    }
+                }else{
+                    printMessage("You Denied Permision");
+                }
+                break;
+        }
+
+    }
+    int fg=0;
+    private void createAlertDialoge() {
+        list=new ArrayList<>();
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(MainActivity.this);
+        myAlert.setTitle("Are you sure you want to delete the amounts?");
+        //myAlert.setMessage("Are you sure you want to delete the amounts ?");
+        myAlert.setMultiChoiceItems(R.array.OS_name, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean isChecked) {
+                String arr[]=getResources().getStringArray(R.array.OS_name);
+
+                if (isChecked){
+                    list.add(arr[i]);
+                    fg=1;
+                }
+                else if (list.contains(arr[i])){
+                    list.remove(arr[i]);
+                    fg=0;
+                }
+            }
+        });
+
+        myAlert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String data="";
+                for (String item:list){
+                    data=item;
+                }
+                if (fg==1){
+                    adAmt.setText("0");
+                    asAmt.setText("0");
+                    baAmt.setText("0");
+                    jeAmt.setText("0");
+                    saveData();
+                    printMessage("Amount Deleted..! please wait..!");
+                    finish();
+                    startActivity(getIntent());
+                }
+                else{
+                    printMessage("Sorry you haven't checked..!");
+                }
+            }
+        });
+        myAlert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                printMessage("Sorry an error occur..!");
+            }
+        });
+        myAlert.show();
+    }
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        adText = sharedPreferences.getString(adTEXT, "0");
+        asText = sharedPreferences.getString(asTEXT, "0");
+        baText = sharedPreferences.getString(baTEXT, "0");
+        jeText = sharedPreferences.getString(jeTEXT, "0");
+    }
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(adTEXT, adAmt.getText().toString());
+        editor.putString(asTEXT, asAmt.getText().toString());
+        editor.putString(baTEXT, baAmt.getText().toString());
+        editor.putString(jeTEXT, jeAmt.getText().toString());
+
+        Date currentTime = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss");
+        String strDate = dateFormat.format(currentTime);
+        editor.putString(curdate, strDate);
+
+        editor.apply();
+
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+        String adData = adAmt.getText().toString();
+        String asData = asAmt.getText().toString();
+        String baData = baAmt.getText().toString();
+        String jeData = jeAmt.getText().toString();
+
+        String wData= strDate+"="+adData+"="+asData+"="+baData+"="+jeData+"*\n";
+        writeData(wData);
+
+    }
+    public void updateViews() {
+        adAmt.setText(adText);
+        asAmt.setText(asText);
+        baAmt.setText(baText);
+        jeAmt.setText(jeText);
+    }
+    private void writeData(String data) {
+        try {
+            File f =new File(Environment.getExternalStoragePublicDirectory("Android/media"),filename);
+            FileWriter fos = new FileWriter(f,true);
+            fos.write(data);
+            fos.close();
+            //printMessage("File Saved to" + getFilesDir() + "/" + filename +" completed...");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+            printMessage("File not found..!!!");
+        }catch (IOException e) {
+            e.printStackTrace();
+            printMessage("Error in saving..!!");
+        }
+    }
+    public void printMessage(String m) {
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    public String addition(String a, String b){
+        double a1=0,b1=0,c=0;
+        a1=Double.parseDouble(a);
+        b1=Double.parseDouble(b);
+        c = b1+a1;
+        String i =Double.toString(c);
+
+        return i;
+    }
+    public String addition(String a, double b){
+        double a1=0.0,c=0.0;
+        a1=Double.parseDouble(a);
+        c=a1+b;
+        String j =Double.toString(c);
+
+        return j;
+    }
+    public String substraction(String a, String b){
+        double a1=0,b1=0,c=0;
+        a1=Double.parseDouble(a);
+        b1=Double.parseDouble(b);
+        c = b1-a1;
+        String h =Double.toString(c);
+
+        return h;
+    }
+    public String substraction(String a, double b){
+        double a1=0.0,c=0.0;
+        a1=Double.parseDouble(a);
+        c=a1-b;
+        String k =Double.toString(c);
+
+        return k;
+    }
+}
---
+++ app/src/main/java/com/example/hostel_pttu_book/MainActivity4.java
@@ -0,0 +1,162 @@
+package com.example.hostel_pttu_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+
+public class MainActivity4 extends AppCompatActivity {
+    TextView dTv,aDv,aSv,bAv,jEv,dTv1,aDv1,aSv1,bAv1,jEv1;
+    Button btn1,fDbtn;
+    SharedPreferences prefs;
+
+    private static final String filename = ".demoFile.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btn1=(Button) findViewById(R.id.btnView);
+        fDbtn=(Button) findViewById(R.id.fDbtn);
+
+        dTv=(TextView) findViewById(R.id.dT);
+        aDv=(TextView) findViewById(R.id.aD);
+        aSv=(TextView) findViewById(R.id.aS);
+        bAv=(TextView) findViewById(R.id.bA);
+        jEv=(TextView) findViewById(R.id.jE);
+        dTv1=(TextView) findViewById(R.id.dT1);
+        aDv1=(TextView) findViewById(R.id.aD1);
+        aSv1=(TextView) findViewById(R.id.aS1);
+        bAv1=(TextView) findViewById(R.id.bA1);
+        jEv1=(TextView) findViewById(R.id.jE1);
+
+        prefs=getSharedPreferences("sharedPrefs",MODE_PRIVATE);
+        String adText=prefs.getString("adText","no values");
+        String asText=prefs.getString("asText","no values");
+        String baText=prefs.getString("baText","no values");
+        String jeText=prefs.getString("jeText","no values");
+        String curdate=prefs.getString("curdate","no values");
+        dTv.setText(curdate);
+        aDv.setText(adText);
+        aSv.setText(asText);
+        bAv.setText(baText);
+        jEv.setText(jeText);
+
+
+        fDbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity4.this);
+                builder.setMessage("Do you want to Delete the file ?");
+                builder.setTitle("Alert !");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    try {
+                        File f = new File(Environment.getExternalStoragePublicDirectory("Android/media"), filename);
+                        if (f.delete()) {
+                            printMessage("File Successfully Deleted..!!");
+                            finish();
+                        } else {
+                            printMessage("Error in Deleting..!!");
+                        }
+                    }catch(Exception e){
+                        e.printStackTrace();
+                        printMessage("File not found..!!");
+                    }
+                });// Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+                builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    printMessage("Cancelled..!");
+                    dialog.cancel();
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readData();
+            }
+        });
+    }
+    public void printMessage(String m) {
+        Toast.makeText(this, m, Toast.LENGTH_LONG).show();
+    }
+
+    private void readData() {
+        String info=null;
+        StringBuilder temp = new StringBuilder();
+        StringBuilder da = new StringBuilder();
+        StringBuilder ad = new StringBuilder();
+        StringBuilder as = new StringBuilder();
+        StringBuilder ba = new StringBuilder();
+        StringBuilder je = new StringBuilder();
+        try {
+            File f = new File(Environment.getExternalStoragePublicDirectory("Android/media"), filename);
+            FileInputStream fin = new FileInputStream(f);
+            InputStreamReader input = new InputStreamReader(fin);
+            BufferedReader br =new BufferedReader(input);
+            while ((info = br.readLine()) != null) {
+                temp.append(info);
+            }
+            String nl = "\n";
+            String temp1 = new String(temp.toString());
+            StringTokenizer st1 = new StringTokenizer(temp1, "*");
+            while (st1.hasMoreTokens()) {
+                String temp2 = new String(st1.nextToken());
+                StringTokenizer st2 = new StringTokenizer(temp2, "=");
+                while (st2.hasMoreTokens()) {
+                    da.append(st2.nextToken());
+                    da.append(nl);
+                    ad.append(st2.nextToken());
+                    ad.append(nl);
+                    ad.append(nl);
+                    as.append(st2.nextToken());
+                    as.append(nl);
+                    as.append(nl);
+                    ba.append(st2.nextToken());
+                    ba.append(nl);
+                    ba.append(nl);
+                    je.append(st2.nextToken());
+                    je.append(nl);
+                    je.append(nl);
+                }
+            }
+            dTv1.setText(da.toString());
+            aDv1.setText(ad.toString());
+            aSv1.setText(as.toString());
+            bAv1.setText(ba.toString());
+            jEv1.setText(je.toString());
+            fin.close();
+            printMessage("reading from file completed..");
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+            printMessage("File Not Found..!!");
+        }catch (IOException e) {
+            e.printStackTrace();
+            printMessage("Error..!!");
+        }
+
+    }
+}
